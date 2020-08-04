f7b750e6a12328b68003472fdda8ae2273146a72
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-08-05 00:46:36 +0900

    クイックスタート

--- Flask_practice/app.py
+++ Flask_practice/app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index, World!'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name="not"):
+    return render_template('hello.html', name=name)
+
+
+
+# $ python app.pyで起動する
+# if __name__ == "__main__":
+#     app.run(debug=True, threaded=True)
