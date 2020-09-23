9a8e5ee6cb65a5195b661e4c534bd2726a53f146
tokky08 <tokky.kz08@gmail.com>
2020-09-23 23:48:49 +0900

    practice

---
+++ Flask_Vue/frontend/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <el-menu :default-active="activeIndex" mode="horizontal" router>
+      <el-menu-item index="home" :route="{ name:'home' }">Home</el-menu-item>
+      <el-menu-item index="about" :route="{ name:'about' }">About</el-menu-item>
+      <!-- <router-link to="/about">ページ２へ</router-link> -->
+      <el-menu-item>
+        <a href="https://element.eleme.io" target="_blank">Link</a>
+      </el-menu-item>
+    </el-menu>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      activeIndex: this.$route.name
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+a {
+  text-decoration: none;
+}
+</style>
